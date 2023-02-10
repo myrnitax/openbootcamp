6aeca2943753b46f2159689951cf6968cd0f93a9
Manuel Ramos La Gambino <manuelramoslg@gmail.com>
2023-02-10 14:23:17 +0100

    Inicio ReactJS

---
+++ REACT INICIAL/hola/src/components/pure/greeting.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class Greeting extends Component {
+  render() {
+    return (
+      <div>
+        <h1>¡Hola!</h1>
+      </div>
+    );
+  }
+}
+
+Greeting.propTypes = {};
+
+export default Greeting;
